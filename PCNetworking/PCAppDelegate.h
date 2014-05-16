a29dcbb25e0059cc3e707a9446e97f04fe27138b
Paul Carpenter <paulcarpenter@streeteasy.com>
2014-05-16 01:23:43 -0400

    Initial Commit

---
+++ PCNetworking/PCAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  PCAppDelegate.h
+//  PCNetworking
+//
+//  Created by Paul Carpenter on 5/16/14.
+//  Copyright (c) 2014 Paul Carpenter. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PCAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ PCNetworking/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  PCNetworking
+//
+//  Created by Paul Carpenter on 5/16/14.
+//  Copyright (c) 2014 Paul Carpenter. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "PCAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([PCAppDelegate class]));
+    }
+}
